ea794dfaeaa4c37f84a5e5ff0de12c2baff9f26f
Angus Gaukroger <agaukroger77@gmail.com>
2023-01-18 21:57:30 +0000

    Pricing section basically there, axios ins+contact

---
+++ src/components/UIElements/ContactForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+function formSubmit()
+{
+    axios.post("www.123.com","testData" );
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
