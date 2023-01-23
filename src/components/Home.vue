6dbf3ad4faefba0960bead1cfe66eda91a4db838
Angus Gaukroger <agaukroger77@gmail.com>
2023-01-23 22:16:38 +0000

    some level of functionality w/ vuex

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
         <LeccionesPrivadas />
         <Contact />
         <Footer />
-
+  
     
 </template>
 
@@ -25,48 +25,101 @@
 
 
 
-    import {computed, onMounted, onBeforeMount} from 'vue';
+    import {computed, onMounted, onBeforeMount,  ref} from 'vue';
     import {useStore} from 'vuex';
-
-    const link = import.meta.env.VITE_LINK;
-
-    const prices = {
-        us: {
-            one_hr_private: 18,
-            two_hr_private: 32,
-            four_hr_private: "",
-            six_hr_private: "",
-            eight_hr_private: "",
-            
-            beginner_group: "",
-            pre_elementary_group: "",
-            elementary_group: "",
-            pre_intermediate_group: "",
-            intermediate_group: "",
-            pre_advanced_group: "",
-
-        }
-    }
-    onBeforeMount(() => {
-
-        fetch(link)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (payload) {
-            console.log(payload.location.country.name);
-            setRegion(payload.location.country.name)
-        });
-
-    })
-
-    function setRegion(region)
-    {
+    // import {i18n} from 'vue-i18n'
         
-    }
     const store = useStore();
 
-    const name = computed(() =>  {
-        return store.state.user.name;
+    const priceList = computed(() =>  {
+        return store.state.region_prices.prices;
     })
+    const link = import.meta.env.VITE_LINK;
+    var localeId = ref("");
+
+    /*Can be pulled from a backend in time*/ 
+    const prices = {
+        us: {
+            region: "us",
+            symbol: "$",
+            one_hr_private: 18,
+            two_hr_private: 32,
+            four_hr_private: 64,
+            six_hr_private: 96,
+            eight_hr_private: 128,
+            
+            beginner_group: 36,
+            pre_elementary_group: 40,
+            elementary_group: 44,
+            pre_intermediate_group: 56,
+            intermediate_group: 52,
+            pre_advanced_group: 40,
+        },
+        en: {
+            region: "en",
+            symbol: "£",
+
+            one_hr_private: 20,
+            two_hr_private: 34,
+            four_hr_private: 66,
+            six_hr_private: 106,
+            eight_hr_private: 444,
+            
+            beginner_group: 40,
+            pre_elementary_group: 44,
+            elementary_group: 56,
+            pre_intermediate_group: 52,
+            intermediate_group: 80,
+            pre_advanced_group: 16,
+        }
+    }
+    
+    onMounted(() => {
+        fetch(link)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (payload) {
+        console.log(payload.location.country.name);
+        setRegion(payload.location.country.name)
+    });
+
+    } )
+
+function setRegion(region)
+{
+    switch(region)
+    {
+        case "Peru":
+            localeId.value = "pe";
+            break;
+        case "Colombia":
+            localeId.value = "co";
+            break;
+        case "United Kingdom":
+            localeId.value = "en";
+            // console.log(prices.en)
+            store.dispatch('updatePrices', {
+                region: prices.en.region,
+                symbol: prices.en.symbol,
+
+                one_hr_private: prices.en.one_hr_private,
+                two_hr_private: prices.en.two_hr_private,
+                four_hr_private: prices.en.four_hr_private,
+                six_hr_private: prices.en.six_hr_private,
+                eight_hr_private: prices.en.eight_hr_private,
+                
+                beginner_group: prices.en.beginner_group,
+                pre_elementary_group: prices.en.pre_elementary_group,
+                elementary_group: prices.en.elementary_group,
+                pre_intermediate_group: prices.en.pre_intermediate_group,
+                intermediate_group: prices.en.intermediate_group,
+                pre_advanced_group: prices.en.pre_advanced_group,
+            });
+            break;
+        
+    }
+    console.log(localeId.value);
+    console.log(store.state.region_prices.prices);
+}
 </script>
