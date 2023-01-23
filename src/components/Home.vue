313453b7be33c31be51f6b01b4a5135eeff2b751
Angus Gaukroger <agaukroger77@gmail.com>
2023-01-23 20:29:02 +0000

    a working version

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -25,9 +25,45 @@
 
 
 
-    import {computed} from 'vue';
+    import {computed, onMounted, onBeforeMount} from 'vue';
     import {useStore} from 'vuex';
 
+    const link = import.meta.env.VITE_LINK;
+
+    const prices = {
+        us: {
+            one_hr_private: 18,
+            two_hr_private: 32,
+            four_hr_private: "",
+            six_hr_private: "",
+            eight_hr_private: "",
+            
+            beginner_group: "",
+            pre_elementary_group: "",
+            elementary_group: "",
+            pre_intermediate_group: "",
+            intermediate_group: "",
+            pre_advanced_group: "",
+
+        }
+    }
+    onBeforeMount(() => {
+
+        fetch(link)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (payload) {
+            console.log(payload.location.country.name);
+            setRegion(payload.location.country.name)
+        });
+
+    })
+
+    function setRegion(region)
+    {
+        
+    }
     const store = useStore();
 
     const name = computed(() =>  {
