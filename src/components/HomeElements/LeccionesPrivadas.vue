a1c7a07c7027d69a10f3a114abad40ea78fcaa15
Angus Gaukroger <agaukroger77@gmail.com>
2023-01-25 22:12:55 +0000

    Private section fix

--- src/components/HomeElements/LeccionesPrivadas.vue
+++ src/components/HomeElements/LeccionesPrivadas.vue
@@ -5,7 +5,7 @@
                 <path d="M1200,0H0V120H281.94C572.9,116.24,602.45,3.86,602.45,3.86h0S632,116.24,923,120h277Z" class="shape-fill"></path>
             </svg>
         </div>
-        <section id="ClasesPrivadas" class="relative h-screen w-full flex flex-col sm:space-y-8 justify-center z-0">
+        <section id="ClasesPrivadas" class="relative my-4 h-screen w-full flex flex-col sm:space-y-8 justify-center z-0">
             <h1 class="text-fade1 sm:text-5xl text-2xl text-center font-bold md:py-2  aos-item" data-aos="fade-left">Clases Privadas</h1> 
             <h2 class="text-fade1 sm:text-3xl text-md text-center md:py-2 aos-item" data-aos="fade-right">Compra un paquete hoy y recibirás tu primera clase de 30 minutos <strong>totalmente gratis!</strong> </h2>
             <div  class="w-full h-100 text-texttone flex flex-row flex-wrap justify-around items-center">         
@@ -16,7 +16,7 @@
                     <Package :currency="store.state.region_prices.prices.symbol" :price="store.state.region_prices.prices.eight_hr_private" :title="titles[3]" :includes="includes[3]" />
              
             </div>
-            <h2 class="text-fade1 sm:text-xl text-md text-center font-bold py-2 aos-item" data-aos="fade-left">Clases individuales cuestan {{ store.state.region_prices.prices.one_hr_private }} por hora.</h2>
+            <h2 class="text-fade1 sm:text-xl text-md text-center font-bold" data-aos="fade-left">Clases individuales cuestan {{ store.state.region_prices.prices.one_hr_private }} por hora.</h2>
            
             
             
--- src/components/UIElements/Package.vue
+++ src/components/UIElements/Package.vue
@@ -1,9 +1,9 @@
 <template>
     <div class=" aos-item" data-aos="fade-right">
-        <div class="p-1 border-4 bg-gradient-to-r rounded-lg from-main to-highlight hover:scale-105 hover:cursor-pointer">
-            <div class="  bg-fade1 p-1 xl:px-5 xl:py-3 xxl:space-y-6   hover:cursor-pointer">
+        <div class="p-1   h-auto sm:h-auto border-4 bg-gradient-to-r rounded-lg from-main to-highlight hover:scale-105 hover:cursor-pointer">
+            <div class=" h-32 sm:h-auto bg-fade1 p-1 xl:px-5 xl:py-3 xxl:space-y-6   hover:cursor-pointer">
                 <div class=" md:space-x-3 xl:py-4 flex flex-row items-end justify-center">
-                    <span class="text-lg xl:text-7xl">{{ price }}</span>
+                    <span class="text-lg xl:text-7xl">{{ price }} </span>
                     <span class="text-lg xl:text-3xl"> x mes</span>
                 </div>
                 <div class=" xl:py-4 text-md xl:text-3xl font-bold flex justify-center">
@@ -13,7 +13,7 @@
                     {{ includes }}
                 </div>
                 <div class="flex justify-center ">
-                    <button class="bg-gradient-to-r from-main to-highlight hover:scale-105 text-texttone rounded-md border-texttone p-1 md:p-3 border-5 ">
+                    <button class="bg-gradient-to-r from-main to-highlight hover:scale-105 text-texttone rounded-md border-texttone  p-2 md:p-3 border-5 ">
                         <strong>
                             Reserva Ahora!
                         </strong>
